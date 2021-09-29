a2176e43e0cba4be135351243e8ee235895df23d
jaycap <jaypodador@gmail.com>
2021-09-29 14:21:13 +0800

    first commit

---
+++ src/app/participants/participants.service.ts
@@ -0,0 +1,15 @@
+export class ParticipantsService {
+    getParticipants(){
+        return [
+            {name: "din tag", age:12 + " years old", gender: "Male"},
+            {name: "moy moy", age:13 + " years old", gender: "Female"},
+            {name: "asu can", age:21 + " years old", gender: "Female"},
+            {name: "john pero", age:22 + " years old", gender: "Female"},
+            {name: "jay pod", age:25 + " years old", gender: "Female"},
+            {name: "lance bayot", age:15 + " years old", gender: "Male"},
+            {name: "layla pokpok", age:11+ " years old", gender: "Female"},
+            {name: "balmond pangit", age:7+ " years old", gender: "Bayot"},
+        ];
+    }
+
+}
